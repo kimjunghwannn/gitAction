7fa69c39c26ca902338ba518f6643e900cf7aa78
kimjunghwannn <aa2002dcks@naver.com>
2025-01-15 23:09:39 +0900

    테스트

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "ㄴ아아니아이ㅣ이ㅏ이니이나이이나ㅏ이ssdsdsdss";
+        return "ㄴ아아니아이ㅣ이ㅏ이니이나이이나ㅏ이ssdsdsdsssdsd";
     }
 }
